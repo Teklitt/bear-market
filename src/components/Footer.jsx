bd44ae5187904902d6780a40bed63fd01ec0512e
Ebun O <remioludemi@gmail.com>
2023-11-30 00:01:11 -0500

    created navbar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import "../css/Footer.css"
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer>
+      <div className='footer-container'>
+        <div className='footer-column'>
+          <p style={{fontWeight:"bold"}}>Get to know us</p>
+          <p>About Bear Market</p>
+          <p>Bear Market Team</p>
+        </div>
+          
+        <div className='footer-column'>
+          <p style={{fontWeight:"bold"}}>Make Money With Us</p>
+          <p>Sell on Bear Market</p>
+          <p>Advertise your Product</p>
+        </div>
+          
+        <div className='footer-column'>
+          <p style={{fontWeight:"bold"}}>Back to top</p>
+          <p>📸🎶</p>
+        </div>
+          
+        <div className='footer-column'>
+          <p style={{fontWeight:"bold"}}>Payment Services</p>
+          <p>Bear Card</p>
+          <p>Gift Card</p>
+        </div>
+          
+        <div className='footer-column'>
+          <p style={{fontWeight:"bold"}}>Let Us Help You</p>
+          <p>Your Account</p>
+          <p>Return & Replacement</p>
+        </div>
+          
+      </div>
+  </footer>
+  ) 
 }
 
 export default Footer
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+import logo from '../images/Bear Market logo.png';
+import "../css/Navbar.css";
+
+
 
 const NavBar = () => {
-  return <div>NavBar</div>
+  return (
+  <nav>
+    <div className='navbar-container'>
+      <img src={logo} className="navbar-logo" alt="logo"/>
+      <p>Deliver to <br></br> 📍 Baltimore 21212</p>
+      <div className='search-container'>
+        <button>Filter ⌄</button>
+        <input
+          type="text"
+          placeholder="Search Bear Market"
+        />
+        <button type="submit">Submit</button>
+      </div>
+      <p>Hello Tariq, <br></br> Account Details ⌄</p>
+      <a href="javascript:void"> Returns & <br></br>Orders</a> 
+      <p>🛒 5</p>
+    </div>
+    <div className='navbar-categories'>
+      <a href="javascript:void">All</a>
+      <a href="javascript:void">School Supplies</a>
+      <a href="javascript:void">Electronics</a>
+      <a href="javascript:void">Books</a>
+      <a href="javascript:void">Clothing</a>
+      <a href="javascript:void">Food</a>
+      <a href="javascript:void">Subscribe & Save</a>
+    </div>
+  </nav>
+  )
 }
 
 export default NavBar
